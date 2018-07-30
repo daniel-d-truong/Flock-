8500d3387eac581f06847dc249fe7113201de4f7
Demo User <demouser@MacBook-Pro.local>
2018-07-30 15:03:11 -0700

    Updating main.py with vars_template and created confirm-event html

--- main.py
+++ main.py
@@ -20,6 +20,24 @@ class HostEventHandler(webapp2.RequestHandler): #making events
         form_template = JINJA_ENVIRONMENT.get_template('templates/form.html')
         self.response.write(form_template.render())
 
+    def post(self):
+        vars_template = {
+            'name': self.request.get('name'),
+            'description': self.request.get('description'),
+            'type': self.request.get('type'),
+            'date': self.request.get('date'),
+            'time_start': self.request.get('time_start'),
+            'time_end': self.request.get('time_end'),
+            'address': self.request.get('address'),
+            'people_needed': self.request.get('people_needed')
+        }
+        confirm_template = JINJA_ENVIRONMENT.get_template('templates/confirm-event.html')
+        self.response.write(form_template.render(vars_template))
+        store = Event(name=vars_template['name'], description = vars_template['description'],
+            type = vars_template['type'], date=vars_template['date'], time_start=vars_template['time_start'],
+            time_end=vars_template['time_end'], address=vars_template['address'], people_needed=vars_template['people_needed'])
+        k = store.put()
+        print (k)
 class FindEventHandler(webapp2.RequestHandler): #newsfeed and searching for events
     def get(self):
         self.response.write('Let\'s find an event!!')
