04c8fc9de0701e7a99a2dd3e7fc2394bf0bcec4d
Demo User <demouser@MacBook-Pro.local>
2018-07-30 11:56:12 -0700

    Created separate models.py and added Users class

--- main.py
+++ main.py
@@ -1,37 +1,33 @@
 import webapp2
 import jinja2
 import os
+from models import Event, User, Relation
 from google.appengine.ext import ndb
-
-class Events(ndb.model):
-    name = ndb.StringProperty(required=True, default = "")
-    title = ndb.StringProperty(required=True, default="")
-    address = ndb.StringProperty(required=True, default="")
-    type = ndb.StringProperty(required=True, default="")
-    date = ndb.DateProperty(required=True, default="")
-    time_start = ndb.TimeProperty(required=True, default="") #need to learn how this works
-    time_end = ndb.TimeProperty(required=True, default="")
-    description = ndb.StringProperty(default = "")
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-class MainPageHandler(webapp2.RequestHandler): #main page
+class WelcomeHandler(webapp2.RequestHandler): #main page
     def get(self):
-        self.response.write('Hello World')
+        welcome_template = JINJA_ENVIRONMENT.get_template('templates/welcome.html')
+        self.response.write(welcome_template.render())
 
 class HostEventHandler(webapp2.RequestHandler): #making events
     def get(self):
         self.response.write('Write down info in order to host your event.')
+        form_template = JINJA_ENVIRONMENT.get_template('templates/form.html')
+        self.response.write(form_template.render())
 
 class FindEventHandler(webapp2.RequestHandler): #newsfeed and searching for events
     def get(self):
         self.response.write('Let\'s find an event!!')
+        newsfeed_template = JINJA_ENVIRONMENT.get_template('templates/newsfeed.html')
+        self.response.write(newsfeed_template.render())
 
 app = webapp2.WSGIApplication([
-    ('/', MainPageHandler),
+    ('/', WelcomeHandler),
     ('/form', HostEventHandler),
     ('/newsfeed', FindEventHandler)
 ], debug=True)
